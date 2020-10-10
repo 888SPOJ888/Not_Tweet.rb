4b17e1b6f562e1f52dca5e523f85e7e489a09f85
Alexis Montealegre <ajmu26@gmail.com>
2020-10-10 16:24:57 -0300

    creando api views

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,14 @@ Rails.application.routes.draw do
   get "hashtags",            to: "hashtags#index",     as: :hashtags
   root to: "tweets#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  scope '/api' do
+    get '/news', to: 'api#news'
+    get '/:date1/:date2', to: 'api#:date1/:date2' 
+    post '/tweets', to: 'api#create_tweet'
+  end
+
+
+
 end
+
